93f252bdb0252846a78c9658a304d5e73b8cf9b8
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:12:01 +0530

    Added PortFolio class

---
+++ PortFolio.cs
@@ -0,0 +1,20 @@
+﻿namespace StockManagement
+{
+    public class PortFolio
+    {
+        public string Symbol;
+        public double InvestedAmount;
+        public int Qty;
+        public double AvgBuy;
+        public double PL;
+
+        public PortFolio(string symbol, double investedAmount, int qty)
+        {
+            Symbol = symbol;
+            InvestedAmount = investedAmount;
+            Qty = qty;
+            AvgBuy = 0;
+            PL = 0;
+        }
+    }
+}

36b5da1fc44bfd2b401e7514d8e42e727879ea13
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:11:55 +0530

    Added Order class

---
+++ Order.cs
@@ -0,0 +1,20 @@
+﻿namespace StockManagement
+{
+    public class Order
+    {
+        public string TypeOfTransaction;
+        public string StockSymbol;
+        public double Price;
+        public int Qty;
+        public DateTime TrasactionTime;
+
+        public Order(string typeOfTransaction, string stockSymbol, double price, int qty)
+        {
+            TypeOfTransaction = typeOfTransaction;
+            StockSymbol = stockSymbol;
+            Price = price;
+            Qty = qty;
+            TrasactionTime = DateTime.Now;
+        }
+    }
+}

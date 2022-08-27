82122171ed83eea8fe9a1fec1f6f158e6842a0c9
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:11:48 +0530

    Added LinkList class

---
+++ LinkList.cs
@@ -0,0 +1,48 @@
+﻿namespace StockManagement
+{
+    public class LinkList
+    {
+        private class Node
+        {
+            public Order Details;
+            public Node Next;
+
+            public Node(Order details)
+            {
+                Details = details;
+            }
+        }
+
+        private Node Head;
+        private Node Tail;
+        private int Size;
+
+        public void AddOrder(Order details)
+        {
+            var newNode = new Node(details);
+
+            if (Head == null)
+                Head = Tail = newNode;
+
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+            Size++;
+        }
+
+        public void OrderHistory()
+        {
+            Console.WriteLine("---------------------------Order histroy---------------------------------------------");
+
+            var temp = Head;
+            while (temp != null)
+            {
+                Console.WriteLine("Date Time : {1}, Type : {2}, Stock : {3}, Price : {4}, Quantity : {5}", temp.Details.TrasactionTime.ToLocalTime(),temp.Details.TypeOfTransaction,temp.Details.StockSymbol,temp.Details.Price,temp.Details.Qty);
+                temp = temp.Next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StockManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new UserInterface().Home();
+        }
+    }
+}
